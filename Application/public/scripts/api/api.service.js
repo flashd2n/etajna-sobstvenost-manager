741902fb383a241e2a73baaefe37753e9bac4ed5
flash <k.kostov.eu@gmail.com>
2017-07-28 19:01:58 +0300

    systemjs set up, now all client scripts run through systemjs + babel transpile

---
+++ Application/public/scripts/api/api.service.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const log = () => {
+    console.log('WORKS');
+};
+
+export {
+    log,
+};
---
+++ Application/public/scripts/system.config.js
@@ -0,0 +1,16 @@
+/* globals SystemJS */
+
+SystemJS.config({
+    transpiler: 'plugin-babel',
+    map: {
+        // system js files
+        'plugin-babel': '/libs/systemjs-plugin-babel/plugin-babel.js',
+        'systemjs-babel-build':
+            '/libs/systemjs-plugin-babel/systemjs-babel-browser.js',
+
+        // app files
+        'home': '/public/scripts/home.js',
+        'chat': '/public/scripts/chat.client.js',
+        'myapartment': '/public/scripts/my.apartment.js',
+    },
+});
