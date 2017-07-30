43ac641463686a63b11ade1cba20704363aba301
flash <k.kostov.eu@gmail.com>
2017-07-30 22:30:14 +0300

    all client side validation is done

--- Application/public/scripts/admin.js
+++ Application/public/scripts/admin.js
@@ -1,4 +1,4 @@
-/* globals $ */
+/* globals $, toastr */
 (() => {
     const callLoader = () => {
         $.loader({
@@ -7,7 +7,20 @@
         });
     };
 
-    $('.approve, .reject, .createFee, .complete-expense').on('click', (evt) => {
+    $('.approve, .reject, .complete-expense').on('click', (evt) => {
         callLoader();
+    });
+
+    $('#fee').on('submit', (evt) => {
+        const month = $('#month').val();
+        const year = $('#year').val();
+        const fee = $('#cost').val();
+
+        if (isNaN(month) || isNaN(year) || isNaN(fee)) {
+            toastr.error(`All inputs must be numbers`);
+            evt.preventDefault();
+        } else {
+            callLoader();
+        }
     });
 })();
--- Application/public/scripts/create.expense.js
+++ Application/public/scripts/create.expense.js
@@ -1,4 +1,4 @@
-/* globals $ */
+/* globals $, toastr */
 (() => {
     const callLoader = () => {
         $.loader({
@@ -7,7 +7,26 @@
         });
     };
 
-    $('.createExpense').on('click', (evt) => {
-        callLoader();
+    $('#create').on('submit', (evt) => {
+        const name = $('#name').val();
+
+        const description = $('#description').val();
+        const cost = $('#cost').val();
+
+        if (name.length === 0) {
+            toastr.error(`The name cannot be empty`);
+
+            evt.preventDefault();
+        } else if (description.length === 0) {
+            toastr.error(`The description cannot be empty`);
+
+            evt.preventDefault();
+        } else if (isNaN(cost)) {
+            toastr.error(`The cost must be a number`);
+
+            evt.preventDefault();
+        } else {
+            callLoader();
+        }
     });
 })();
